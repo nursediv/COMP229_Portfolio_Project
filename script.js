2aa98503a84f807339c1f0fa515c9ff530ea03bb
Divya <129140048+nursediv@users.noreply.github.com>
2023-10-10 11:58:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/* 
+File name: script.js
+Student’s Name: Divya Patel
+StudentID: 301309543
+Date: Oct/7/2023 
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
