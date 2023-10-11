5310287b38f6c99ff86034cacaa4771e65cab45e
Divya <129140048+nursediv@users.noreply.github.com>
2023-10-10 21:21:04 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const routes = require('./routes/index');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Setting up the public directory for static files
-app.use(express.static(path.join(__dirname, 'public')));
+var indexRouter = require('../app/routes/index');
 
-// Setting up the views directory
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');  
+var app = express();
 
-// Use the routes
-app.use('/', routes);
+// view engine setup
+app.set('views', path.join(__dirname, '../app/views'));
+app.set('view engine', 'ejs');
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:3000');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  console.log(err);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
